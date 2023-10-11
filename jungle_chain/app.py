f676c7ae63413a356ba7a4fa4753f08e29a0012b
bckim9489 <bckim9489@gmail.com>
2023-10-11 22:14:04 +0900

    김병철 통합(2023-10-11)

--- jungle_chain/app.py
+++ jungle_chain/app.py
@@ -1,14 +1,46 @@
-from flask import Flask, render_template
+from bson.objectid import ObjectId
+from pymongo import MongoClient
+from flask import Flask, render_template, jsonify, request
+from flask.json.provider import JSONProvider
+import json
+
 app = Flask(__name__)
+client = MongoClient('mongodb://test:test@13.124.58.16',27017)
+db = client.dbjungle
+
+class CustomJSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, ObjectId):
+            return str(o)
+        return json.JSONEncoder.default(self, o)
+
+class CustomJSONProvider(JSONProvider):
+    def dumps(self, obj, **kwargs):
+        return json.dumps(obj, **kwargs, cls=CustomJSONEncoder)
+
+    def loads(self, s, **kwargs):
+        return json.loads(s, **kwargs)
+    
+app.json = CustomJSONProvider(app)
+
 
 # URL 별로 함수명이 같거나,
 # route('/') 등의 주h소가 같으면 안됩니다.
 
+#로그인 페이지
+@app.route('/')
+def login():
+    return render_template('login.html')
 
+#회원가입 페이지
 @app.route('/signup')
-def home():
+def signup():
     return render_template('signup.html')
 
+#메인 페이지
+@app.route('/main')
+def main():
+    return render_template('main.html', name="정글이", id="jungle02")
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
